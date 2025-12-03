b79cb71c857a9591c20a18ae6ee2f452aac2ebcf
Антон Некрасов <nas_box@rambler.ru>
2025-12-03 12:20:26 +0300

    update validate

--- src/App.vue
+++ src/App.vue
@@ -1,145 +1,139 @@
 <script setup>
-import { Form, Field, ErrorMessage } from 'vee-validate'
-import { ref } from 'vue'
-import * as yup from 'yup'
+import { ref } from 'vue';
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import * as yup from 'yup';
 
-const formRef = ref(null)
-const formValid = ref(false)
+const cities = ref([{
+  key: 'msc',
+  value: 'Москва'
+},{
+  key: 'spb',
+  value: 'Санкт-Петербург'
+},
+  {
+  key: 'ovb',
+  value: 'Новосибирск'
+},
+  {
+  key: 'svx',
+  value: 'Екатеринбург'
+},
+  {
+  key: 'other',
+  value: 'Другой'
+}]);
+
+const registrationSuccess = ref(false);
+
+const togglePasswordVisibility = () => {
+  const passwordField = document.getElementById('password');
+  passwordField.type = passwordField.type === 'password' ? 'text' : 'password';
+};
+
+const toggleConfirmPasswordVisibility = () => {
+  const passwordField = document.getElementById('confirm-password');
+  passwordField.type = passwordField.type === 'password' ? 'text' : 'password';
+};
+
+const register = (values) => {
+  registrationSuccess.value = true;
+  // Здесь можно добавить логику для отправки данных на сервер
+  console.log('Registration data:', values);
+};
 
 const schema = yup.object({
-  firstname: yup.string().required('Имя обязательно для заполнения'),
-  lastname: yup.string().required('Фамилия обязательна для заполнения'),
-  country: yup.string().required('Страна обязательна для заполнения'),
-  city: yup.string().required('Город обязателен для заполнения'),
-  phone: yup
-    .string()
-    .required('Телефон обязательный для заполнения')
-    .matches(/^\+?[0-9]{10,15}$/, 'Введите правильный телефон'),
-  email: yup
-    .string()
-    .required('Телефон обязателен для заполнения')
-    .email('Введите корректный Email'),
-  password: yup
-    .string()
-    .required('Введите пароль')
-    .min(4, 'Пароль должен быть не короче 4 символов'),
-  // .matches(/[A-Za-z]/, 'Пароль должен содержать буквы')
-  // .matches(/[0-9]/, 'Пароль должен содержать цифры'),
-  confirmPassword: yup
-    .string()
-    .required('Повторите пароль')
-    .oneOf([yup.ref('password')], 'Пароли не совпадают'),
+  firstname: yup.string().required('Имя обязательно') ,
+  lastname: yup.string().required('Фамилия обязательна'),
+  country: yup.string().required('Страна/Регион обязательны'),
+  city: yup.string().required('Город обязателен'),
+  phone: yup.string().required('Телефон обязателен').matches(/^\+?\d{10,15}$/, 'Неверный формат телефона'),
+  email: yup.string().email('Неверный формат email').required('Email обязателен'),
+  password: yup.string().min(6, 'Пароль должен содержать минимум 6 символов').required('Пароль обязателен'),
+  confirmPassword: yup.string().oneOf([yup.ref('password'), null], 'Пароли не совпадают').required('Подтверждение пароля обязательно'),
   comments: yup.string(),
-  terms: yup
-    .boolean()
-    .transform((value) => value === 'on')
-    .oneOf([false], 'Вы должны принять условия'),
-})
-
-const onFormSubmit = (values) => {
-  // alert('Форма отправлена')
-  formValid.value = true
-  console.log(values)
-}
+  terms: yup.bool().required('Вы должны согласиться с условиями пользования и политикой конфиденциальности.'),
+});
 </script>
 
 <template>
   <div class="container">
     <h1 class="title">Регистрация</h1>
-    <Form
-      ref="formRef"
-      class="registration-form"
-      :validation-schema="schema"
-      @submit="onFormSubmit"
-    >
+    <Form  v-if="!registrationSuccess" class="registration-form" @submit="register" :validation-schema="schema">
       <div class="form-group">
         <label class="form-label" for="firstname">Имя *</label>
-        <Field class="form-control" name="firstname" type="text" id="firstname" />
-        <ErrorMessage name="firstname" class="message--error" />
+        <Field class="form-control" type="text" id="firstname" name="firstname" />
+        <ErrorMessage name="firstname" class="message message--error" />
       </div>
       <div class="form-group">
         <label class="form-label" for="lastname">Фамилия *</label>
-        <Field class="form-control" name="lastname" type="text" id="lastname" />
-        <ErrorMessage name="lastname" class="message--error" />
+        <Field class="form-control" type="text" id="lastname" name="lastname" />
+        <ErrorMessage name="lastname" class="message message--error" />
       </div>
       <div class="form-group">
         <label class="form-label" for="country">Страна/Регион *</label>
-        <Field class="form-control" name="country" type="text" id="country" />
-        <ErrorMessage name="country" class="message--error" />
+        <Field class="form-control" type="text" id="country" name="country" />
+        <ErrorMessage name="country" class="message message--error" />
       </div>
       <div class="form-group">
         <label class="form-label" for="city">Город *</label>
         <div class="custom-select">
-          <Field as="select" class="form-control" id="city" name="city" required>
+          <Field as="select" class="form-control" id="city" name="city">
             <option value="" disabled selected>Выберите город</option>
-            <option value="msc">Москва</option>
-            <option value="spb">Санкт-Петербург</option>
-            <option value="ovb">Новосибирск</option>
-            <option value="svx">Екатеринбург</option>
-            <option value="other">Другой</option>
+            <option v-for="city in cities" :key="city.key" :value="city.value">{{ city.value }}</option>
           </Field>
-          <ErrorMessage name="city" class="message--error" />
         </div>
+        <ErrorMessage name="city" class="message message--error" />
       </div>
       <div class="form-group">
         <label class="form-label" for="phone">Телефон *</label>
-        <Field class="form-control" type="tel" name="phone" id="phone" />
-        <ErrorMessage name="phone" class="message--error" />
+        <Field class="form-control" type="tel" id="phone" name="phone" />
+        <ErrorMessage name="phone" class="message message--error" />
       </div>
       <div class="form-group">
         <label class="form-label" for="email">Email *</label>
-        <Field class="form-control" type="email" name="email" id="email" />
-        <ErrorMessage name="email" class="message--error" />
+        <Field class="form-control" type="email" id="email" name="email" />
+        <ErrorMessage name="email" class="message message--error" />
       </div>
       <div class="form-group form-group--password">
         <label class="form-label" for="password">Пароль *</label>
-        <Field class="form-control" type="password" name="password" id="password" />
-        <button class="btn-icon btn-icon--password" type="button">
+        <Field class="form-control" type="password" id="password" name="password" />
+        <button class="btn-icon btn-icon--password" type="button" @click="togglePasswordVisibility">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-            <path
-              d="M12 4.5C6.5 4.5 2 9 2 12s4.5 7.5 10 7.5S22 14.5 22 12s-4.5-7.5-10-7.5zm0 13c-3.1 0-5.5-2.4-5.5-5.5S8.9 7.5 12 7.5s5.5 2.4 5.5 5.5S15.1 17.5 12 17.5z"
-            />
-            <path d="M12 10c-1.1 0-2 .9-2 2s.9 2 2 2c1.1 0 2-.9 2-2s-.9-2-2-2z" />
+            <path d="M12 4.5C6.5 4.5 2 9 2 12s4.5 7.5 10 7.5S22 14.5 22 12s-4.5-7.5-10-7.5zm0 13c-3.1 0-5.5-2.4-5.5-5.5S8.9 7.5 12 7.5s5.5 2.4 5.5 5.5S15.1 17.5 12 17.5z"/>
+            <path d="M12 10c-1.1 0-2 .9-2 2s.9 2 2 2c1.1 0 2-.9 2-2s-.9-2-2-2z"/>
           </svg>
         </button>
-        <ErrorMessage name="password" class="message--error" />
+        <ErrorMessage name="password" class="message message--error" />
       </div>
       <div class="form-group form-group--password">
-        <label class="form-label" for="confirmPassword">Подтвердите пароль *</label>
-        <Field
-          class="form-control"
-          type="password"
-          name="confirmPassword"
-          id="confirmPassword"
-          validate-on-input
-          :validateOnValueUpdate="true"
-        />
-        <button class="btn-icon btn-icon--password" type="button">
+        <label class="form-label" for="confirm-password">Подтвердите пароль *</label>
+        <Field class="form-control" type="password" id="confirm-password" name="confirmPassword" />
+        <button class="btn-icon btn-icon--password" type="button" @click="toggleConfirmPasswordVisibility">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-            <path
-              d="M12 4.5C6.5 4.5 2 9 2 12s4.5 7.5 10 7.5S22 14.5 22 12s-4.5-7.5-10-7.5zm0 13c-3.1 0-5.5-2.4-5.5-5.5S8.9 7.5 12 7.5s5.5 2.4 5.5 5.5S15.1 17.5 12 17.5z"
-            />
-            <path d="M12 10c-1.1 0-2 .9-2 2s.9 2 2 2c1.1 0 2-.9 2-2s-.9-2-2-2z" />
+            <path d="M12 4.5C6.5 4.5 2 9 2 12s4.5 7.5 10 7.5S22 14.5 22 12s-4.5-7.5-10-7.5zm0 13c-3.1 0-5.5-2.4-5.5-5.5S8.9 7.5 12 7.5s5.5 2.4 5.5 5.5S15.1 17.5 12 17.5z"/>
+            <path d="M12 10c-1.1 0-2 .9-2 2s.9 2 2 2c1.1 0 2-.9 2-2s-.9-2-2-2z"/>
           </svg>
         </button>
-        <ErrorMessage name="confirmPassword" class="message--error" />
+        <ErrorMessage name="confirmPassword" class="message message--error" />
       </div>
       <div class="form-group form-group--full-width">
         <label class="form-label" for="comments">Дополнительная информация</label>
-        <Field as="textarea" class="form-control" id="comments" name="comments"></Field>
-        <ErrorMessage name="comments" class="message--error" />
+        <Field as="textarea" class="form-control" id="comments" name="comments" />
+        <ErrorMessage name="comments" class="message message--error" />
       </div>
       <div class="form-group form-group--full-width">
         <label class="form-label form-label--checkbox" for="terms">
-          <Field type="checkbox" id="terms" name="terms" />
+          <Field type="checkbox" id="terms" name="terms" :value="true" />
           Я согласен c условиями пользования и политикой конфиденциальности
         </label>
-        <ErrorMessage name="terms" class="message--error" />
+        <ErrorMessage name="terms" class="message message--error" />
       </div>
       <button class="btn" type="submit">Зарегистрироваться</button>
-      <button @click="formRef.value.resetForm()" class="btn" type="reset">Очистить форму</button>
+      <button class="btn" type="reset">Очистить форму</button>
     </Form>
-    <div v-if="formValid" class="message message--success">Регистрация прошла успешно!</div>
+    <div v-else class="message message--success">
+      Регистрация прошла успешно!
+    </div>
   </div>
 </template>
 
@@ -173,9 +167,9 @@ body {
 }
 
 .form-group {
+  position: relative;
   flex-basis: calc(50% - 2rem);
   margin-bottom: 1rem;
-  position: relative;
 }
 
 @media (max-width: 850px) {
@@ -195,16 +189,15 @@ body {
 .form-label {
   display: block;
   font-size: 1.2rem;
-  margin-bottom: 0.75rem;
+  margin-bottom: .75rem;
 }
 
-.form-control,
-.v-select {
+.form-control {
   width: 100%;
   font-size: 1rem;
   padding: 1rem;
-  background-color: #f6f6f6;
-  border: 1px solid #f6f6f6;
+  background-color: #F6F6F6;
+  border: 1px solid #F6F6F6;
   border-radius: 8px;
 }
 
@@ -214,8 +207,7 @@ body {
 
 .custom-select::after {
   content: '';
-  /* background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M7 10l5 5 5-5z"/></svg>')
-    no-repeat center; */
+  background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M7 10l5 5 5-5z"/></svg>') no-repeat center;
   background-size: 30px;
   width: 30px;
   height: 30px;
@@ -260,7 +252,7 @@ textarea.form-control {
 }
 
 .btn {
-  background-color: #f6f6f6;
+  background-color: #F6F6F6;
   padding: 1rem 2rem;
   font-size: 1.2rem;
   border: none;
@@ -273,8 +265,8 @@ textarea.form-control {
   opacity: 0.8;
 }
 
-.btn[type='submit'] {
-  background-color: #8a33fd;
+.btn[type="submit"] {
+  background-color: #8A33FD;
   color: white;
 }
 
@@ -283,28 +275,31 @@ textarea.form-control {
   align-items: center;
   font-size: 1.2rem;
 }
+
 .form-label--checkbox input {
   margin-right: 1rem;
 }
-.form-label--checkbox input[type='checkbox'] {
+
+.form-label--checkbox input[type="checkbox"] {
   width: 20px;
   height: 20px;
 }
-.form-label--checkbox input[type='checkbox']:checked {
-  background-color: #8a33fd;
+
+.form-label--checkbox input[type="checkbox"]:checked {
+  background-color: #8A33FD;
 }
 
-.form-label--checkbox input[type='checkbox']:checked:before {
+.form-label--checkbox input[type="checkbox"]:checked:before {
   content: '';
   display: block;
   width: 20px;
   height: 20px;
-  background-color: #8a33fd;
+  background-color: #8A33FD;
 }
 
 .message {
   margin: 0;
-  font-size: 0.75rem;
+  font-size: .75rem;
   text-align: center;
   position: absolute;
   left: 0;
@@ -318,6 +313,6 @@ textarea.form-control {
 }
 
 .message--error {
-  color: #f44336;
+  color: #F44336;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
-import 'vue-select/dist/vue-select.css'
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-import vSelect from 'vue-select'
 
-const app = createApp(App)
-app.component('v-select', vSelect)
-app.mount('#app')
+createApp(App).mount('#app')
